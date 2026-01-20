32588b354b7a9777f8c68e2379b676e4c7d4e5c4
Musfiqur Jahin <119739345+musfiqurjahin@users.noreply.github.com>
2026-01-20 20:04:44 +0600

    Deploy

---
+++ search.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    bool found = false;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            cout << "Element found at index: " << i << endl;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        cout << "Element not found in the array." << endl;
+    }
+
+    return 0;
+}
